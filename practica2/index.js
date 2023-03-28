afb73e255a50d35aef70ce3ced7dbdd944526214
Loki4ng3l <Lokillo1606@outlook.com>
2023-03-28 09:46:11 -0600

    practica7

---
+++ practica2/index.js
@@ -0,0 +1,8 @@
+let resultado = document.getElementById('resultado');
+
+const asignar = (valor) => resultado.value += valor;
+
+const calcular = () =>{
+    if(resultado.value!='')
+        resultado.value = eval(resultado.value);
+}
