653db486393a9853546aee26c5538e579f9665d4
Loki4ng3l <Lokillo1606@outlook.com>
2023-05-17 09:44:42 -0600

    Práctica 41

---
+++ practica41/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import HolaMundo from './components/HolaMundo.vue'
+</script>
+
+<template>
+  <div>
+    <HolaMundo mensaje="1er componente de VueJS" color="bg-success" letra="fs-5"></HolaMundo>
+
+    <HolaMundo mensaje="2do componente de VueJS"/>
+
+    <hola-mundo mensaje="KEBAB CASE de VueJS"></hola-mundo>
+
+    <holaMundo mensaje="CAMEL CASE de VueJS" color="bg-danger"></holaMundo>
+  </div>
+</template>
