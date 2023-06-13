72692c80bcbb112043e4b11b36d8bb37467a5801
Loki4ng3l <Lokillo1606@outlook.com>
2023-06-12 20:02:49 -0600

    Spotify con Vue

--- SpotifyVue/js/app.js
+++ SpotifyVue/js/app.js
@@ -1,13 +1,60 @@
 // Url's.
-const artistsUrl = "https://api.spotify.com/v1/search?type=artist";
-const tokenUrl = "https://accounts.spotify.com/api/token";
-const albumsUrl = "https://api.spotify.com/v1/artists/artistId/albums";
-const UniqueAlbumUrl = "https://api.spotify.com/v1/albums/albumId";
+const urls = {
+  artists: "https://api.spotify.com/v1/search?type=artist",
+  token: "https://accounts.spotify.com/api/token",
+  albums: (artistId) => `https://api.spotify.com/v1/artists/${artistId}/albums`,
+  album: (albumId) => `https://api.spotify.com/v1/albums/${albumId}`,
+};
 
 // Keys.
 const clientId = "f9dcaf7741d7456fadb358d5622d4b10";
 const clientSecret = "bf9108b14a1145c391cd178112016210";
 const encodedData = btoa(`${clientId}:${clientSecret}`);
+
+async function fetchWithToken(url, token, method = "GET") {
+  const options = {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+  const request = await fetch(url, options);
+  return request.json();
+}
+
+async function getAccessToken() {
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encodedData}`,
+      },
+      body: "grant_type=client_credentials",
+    };
+    const request = await fetch(urls.token, options);
+    const response = await request.json();
+    return response.access_token;
+  } catch (e) {
+    console.error("Error", e.message);
+  }
+}
+
+async function getArtist(name, token) {
+  const url = `${urls.artists}&q=${name}`;
+  return fetchWithToken(url, token);
+}
+
+async function getAlbums(artistId, token) {
+  const url = urls.albums(artistId);
+  return fetchWithToken(url, token);
+}
+
+async function getAlbum(albumId, token) {
+  const url = urls.album(albumId);
+  return fetchWithToken(url, token);
+}
 
 // App.
 const app = new Vue({
@@ -20,83 +67,39 @@ const app = new Vue({
     songs: [],
   },
   methods: {
-    submit: async function () {
+    async submit() {
       try {
-        //Reset
-        this.artist = {};
-        this.album = {};
-        this.albumUrl = "";
-        this.songs = [];
+        this.reset();
 
-        // Get access token.
-        const accessToken = await this.getAccessToken();
+        const accessToken = await getAccessToken();
 
-        // Set URL.
-        const tempArtistsUrl = `${artistsUrl}&q=${this.name}`;
-
-        // Options.
-        const options = {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-        };
-
-        // Get list of artists, save autor ID and info.
-        let request = await fetch(tempArtistsUrl, options);
-        let response = await request.json();
+        let response = await getArtist(this.name, accessToken);
         const artistId = response.artists.items[0].id;
         this.artist = response.artists.items[0];
 
-        // Get artist albums and save ID.
-        const tempAlbumsUrl = albumsUrl.replace("artistId", artistId);
-        request = await fetch(tempAlbumsUrl, options);
-        response = await request.json();
+        response = await getAlbums(artistId, accessToken);
         const albumId = response.items[0].id;
 
-        // Get first album and save its info.
-        const tempUniqueAlbumUrl = UniqueAlbumUrl.replace("albumId", albumId);
-        request = await fetch(tempUniqueAlbumUrl, options);
-        response = await request.json();
+        response = await getAlbum(albumId, accessToken);
         this.album = response;
         console.log(response);
 
-        // Save album URL.
         this.albumUrl = response.images[0].url;
-
-        // Save songs.
         this.songs = response.tracks.items;
 
-        // Reset.
-        this.name = "";
+        this.resetName();
       } catch (e) {
         console.error("Error", e.message);
       }
     },
-    getAccessToken: async function () {
-      try {
-        // Options.
-        const options = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ${encodedData}`,
-          },
-          body: "grant_type=client_credentials",
-        };
-
-        // Request.
-        const request = await fetch(tokenUrl, options);
-        const response = await request.json();
-
-        // Token.
-        const accessToken = response.access_token;
-
-        return accessToken;
-      } catch (e) {
-        console.error("Error", e.message);
-      }
+    reset() {
+      this.artist = {};
+      this.album = {};
+      this.albumUrl = "";
+      this.songs = [];
+    },
+    resetName() {
+      this.name = "";
     },
   },
 });
